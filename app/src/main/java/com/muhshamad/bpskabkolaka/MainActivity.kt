3cce6cf4c1aa6247db050f773a67cc7c85d958dc
statistician7741 <shamad2402@gmail.com>
2020-01-07 11:19:51 +0800

    init 2

--- app/src/main/java/com/muhshamad/bpskabkolaka/MainActivity.kt
+++ app/src/main/java/com/muhshamad/bpskabkolaka/MainActivity.kt
@@ -7,6 +7,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //ok
         setContentView(R.layout.activity_main)
     }
 }
